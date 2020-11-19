5df73df003412c5202e97d44f5cab99dd1bcf9e7
Simeon Schaub <simeondavidschaub99@gmail.com>
2020-11-19 22:50:08 +0100

    it kinda works ?!?

---
+++ model.jl
@@ -0,0 +1,109 @@
+using Flux, Zygote
+using Flux: crossentropy, Data.DataLoader
+using HDF5, DataFrames
+using WeightedOnlineStats
+using Random
+using CUDA
+
+_a = CuArray{Float32,0}(undef)
+
+function accuracy(_a, ŷ, y::Flux.OneHotMatrix)
+    _a[] = count(axes(ŷ, 2)) do i
+        y[argmax(view(ŷ, :, i)), i]
+    end / Float32(size(ŷ, 2))
+    nothing
+end
+
+function step!(model, ds_train, ds_test, loss_function, opt)
+    ps = Flux.params(model)
+
+    trainmode!(model)
+
+    train_loss, train_accuracy = WeightedMean(), WeightedMean()
+    for (x, y, weights) in ds_train
+        local ŷ
+        loss, pb = Zygote.pullback(ps) do
+            ŷ = model(x)
+            return loss_function(ŷ, y; weights)
+        end
+        gs = pb(one(loss))
+        Flux.update!(opt, ps, gs)
+        fit!(train_loss, loss, size(y, 2))
+        @cuda threads=size(ŷ, 2) accuracy(_a, ŷ, y)
+        fit!(train_accuracy, _a[], size(y, 2))
+    end
+
+    testmode!(model)
+
+    test_loss, test_accuracy = WeightedMean(), WeightedMean() 
+    for (x, y, weights) in ds_test
+        ŷ = model(x)
+        loss = loss_function(ŷ, y; weights)
+        fit!(test_loss, loss, size(y, 2))
+        @cuda threads=size(ŷ, 2) accuracy(_a, ŷ, y)
+        fit!(test_accuracy, _a[], size(y, 2))
+    end
+
+    @show value.([train_loss, train_accuracy])
+    @show value.([test_loss, test_accuracy])
+    nothing
+end
+
+basedir = "/ceph/larmbruster/multiclassJAN/bkg_merging/cTag_infos/v1/"
+h5files = joinpath.(basedir, ["Hbb_dnn.h5", "Zbb_dnn.h5"])
+
+using Pandas
+
+function extract_cols(names)
+    return mapreduce(hcat, h5files) do f
+        df = read_hdf(f)
+        PermutedDimsArray(Array(df[names]), (2, 1))
+    end
+end
+
+include("variables.jl")
+
+inputs = extract_cols(Vars.variables["ge4j_ge3t"])
+outputs = Symbol.(extract_cols(["class_label"]))
+weights = prod(Float32, extract_cols(Vars.weights); dims=1)
+
+test_split = 0.25
+n_train = floor(Int, length(outputs) * (1 - test_split))
+
+idx = shuffle(eachindex(outputs))
+train_idx, test_idx = idx[1:n_train], idx[n_train+1:end]
+
+ds_train = (
+    inputs[:, train_idx],
+    Flux.onehotbatch(outputs[train_idx], [:Hbb, :Zbb]),
+    weights[:, train_idx],
+) .|> gpu
+ds_train = DataLoader(ds_train, batchsize=128, shuffle=true)
+
+ds_test = (
+    inputs[:, test_idx],
+    Flux.onehotbatch(outputs[test_idx], [:Hbb, :Zbb]),
+    weights[:, test_idx],
+) .|> gpu
+ds_test = DataLoader(ds_test, batchsize=128, shuffle=false)
+
+n_input, n_output = size(inputs, 1), 2
+n_hidden = [200, 200, 200]
+
+_leakyrelu(x) = max(x * 0.3f0, x)
+
+model = Chain(
+    Dropout(0.1),
+    foldl(n_hidden; init=(Chain(), n_input)) do (c, n_in), n_out
+        Chain(c, Dense(n_in, n_out, _leakyrelu)), n_out
+    end[1],
+    Dense(n_hidden[end], n_output),
+) |> gpu
+
+opt = ADAGrad()
+
+wmean(x, weights) = sum(x .* weights ./ sum(weights))
+
+loss(ŷ, y; weights) = Flux.logitcrossentropy(ŷ, y; agg = x -> wmean(x, weights))
+
+@time Flux.@epochs 100 step!(model, ds_train, ds_test, loss, opt)
