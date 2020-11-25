7d8dfbddda0c6b2feca72e6594823c04ec668533
Simeon Schaub <simeondavidschaub99@gmail.com>
2020-11-25 14:04:53 +0100

    change output features hist

--- script/plots.jl
+++ script/plots.jl
@@ -10,20 +10,16 @@ layer_params = DataFrame(Arrow.Table(joinpath(output_dir, "layer_params.arrow"))
 all_features = DataFrame(Arrow.Table(joinpath(output_dir, "all_features.arrow")))
 
 for ((kind,), df) in pairs(groupby(all_features, :kind))
-    fig, axes = subplots(ncols=2, nrows=1, figsize=(10, 5))
-    _df = select(df, :, :output_predicted_Hbb => (x -> ifelse.(x .> .5, :Hbb, :Zbb)) => :output_predicted)
-    foreach(pairs(groupby(_df, :output_predicted, sort=true)), axes) do ((class,), df), ax
-        idx_Hbb = df.output_expected .=== :Hbb#class
+    fig, ax = subplots()
+    foreach(pairs(groupby(df, :output_expected, sort=true))) do ((class,), df)
         ax.hist(
-            [df[idx_Hbb, Symbol(:output_predicted_, class)], df[.!idx_Hbb, Symbol(:output_predicted_, class)]],
-            weights = [df[idx_Hbb, :weights], df[.!idx_Hbb, :weights]] ./ sum(_df.weights),
-            label=["true Hbb", "true Zbb"], stacked=true, #density=true,
-            bins=20, rwidth=.8
+            df.output_predicted_Hbb,
+            label="true $class", density=true,
+            bins=30, histtype=:step,
         )
-        ax.set_title("classified as $class")
-        ax.set_xlabel("p($class)")
-        ax.legend()
     end
+    ax.set_xlabel("p(Hbb)")
+    ax.legend()
     fig.suptitle(kind)
     fig.savefig(joinpath(output_dir, "output_features_hist_$kind.pdf"))
 
@@ -31,17 +27,18 @@ for ((kind,), df) in pairs(groupby(all_features, :kind))
     for r in eachrow(df)
         i = r.output_expected === :Hbb ? 1 : 2
         j = r.output_predicted_Hbb > .5 ? 1 : 2
-        confusion_mat[i, j] += r.weights_norm
+        confusion_mat[i, j] += r.weights
     end
+    confusion_mat ./= sum(confusion_mat, dims=2)
     fig, ax = subplots()
     img = ax.imshow(confusion_mat)
     for i in 1:2, j in 1:2
         ax.text(j-1, i-1, @sprintf("%.3g", confusion_mat[i, j]), ha=:center, va=:center)
     end
     ax.set_xlabel("predicted output")
-    ax.set_xticks(0:1, [:Hbb,:Zbb])
+    xticks(0:1, [:Hbb,:Zbb])
     ax.set_ylabel("true output")
-    ax.set_yticks(0:1, [:Hbb,:Zbb])
+    yticks(0:1, [:Hbb,:Zbb])
     fig.colorbar(img)
     fig.suptitle(kind)
     fig.savefig(joinpath(output_dir, "confusion_matrix_$kind.pdf"))
