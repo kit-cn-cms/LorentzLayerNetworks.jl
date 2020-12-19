9290792b849bef223dc3cd516d92f1e074e9c200
Simeon Schaub <simeondavidschaub99@gmail.com>
2020-12-19 14:11:19 +0100

    It works!

--- lola_kernel.jl
+++ lola_kernel.jl
@@ -14,20 +14,60 @@ end
 Adapt.adapt_structure(T, a::ArgMinArray) = ArgMinArray(adapt(T, a.idx), adapt(T, a.min))
 Tullio.storage_type(a::ArgMinArray) = typeof(a.min)
 
-argmin_inner(x, y) = ifelse(isless(x[2], y[2]), x, y);
+argmin_inner(x, y) = ifelse(isless(last(x), last(y)), x, y);
+
+d_kk(j, m) = :(
+    (k[4, $(j...)] - k[4, $(m...)])^2 - (k[1, $(j...)] - k[1, $(m...)])^2 - (k[2, $(j...)] - k[2, $(m...)])^2 - (k[3, $(j...)] - k[3, $(m...)])^2
+)
 
 kernel_wd_vec = :(
-    f[m] = w[j, m] *
-        ((k[j, 4] - k[m, 4])^2 - (k[j, 1] - k[m, 1])^2 - (k[j, 2] - k[m, 2])^2 - (k[j, 3] - k[m, 3])^2)
+    f[m] = w[j, m] * $(d_kk([:j], [:m]))
 )
 kernel_wd_mat = :(
-    f[m, l] = w[j, m] *
-        ((k[j, l, 4] - k[m, l, 4])^2 - (k[j, l, 1] - k[m, l, 1])^2 - (k[j, l, 2] - k[m, l, 2])^2 - (k[j, l, 3] - k[m, l, 3])^2)
+    f[m, l] = w[j, m] * $(d_kk([:j, :l], [:m, :l]))
 )
 
-for red in [:+, :min], (N, kernel) in [1 => kernel_wd_vec, 2 => kernel_wd_mat]
-    @eval wd!(f::AbstractArray{<:Any,$N}, w, k::AbstractArray{<:Any,$(N+1)}, ::typeof($red)) = @tullio ($red) $kernel
-end
+kernel_wd_argmin_vec = :(
+    f[m] = (j, w[j, m] * $(d_kk([:j], [:m])))
+)
+kernel_wd_argmin_mat = :(
+    f[m, l] = (j, w[j, m] * $(d_kk([:j, :l], [:m, :l])))
+)
 
-@eval function wd_pullback!(f)
+g(μ) = ifelse(μ == 4, 1, -1)
+kernel_wd_dk_vec = :(
+    dk[μ, i] = 2g(μ) * Δ[m] * w[j_min[m], m] * ((i == j_min[m]) - (i == m)) * (k[μ, j_min[m]] - k[μ, m])
+)
+kernel_wd_dk_mat = :(
+    dk[μ, i, l] = 2g(μ) * Δ[m] * w[j_min[m, l], m] * ((i == j_min[m, l]) - (i == m)) * (k[μ, j_min[m, l], l] - k[μ, m, l])
+)
+
+kernel_wd_dw_vec = :(
+    dw[i, h] = (i == j_min[h]) * Δ[h] * $(d_kk([:(j_min[h])], [:h]))
+)
+kernel_wd_dw_mat = :(
+    dw[i, h] = (i == j_min[h, l]) * Δ[h] * $(d_kk([:(j_min[h, l]), :l], [:h, :l]))
+)
+
+
+for (N, kernel, kernel_argmin, kernel_dk, kernel_dw) in [
+    (1, kernel_wd_vec, kernel_wd_argmin_vec, kernel_wd_dk_vec, kernel_wd_dw_vec),
+    (2, kernel_wd_mat, kernel_wd_argmin_mat, kernel_wd_dk_mat, kernel_wd_dw_mat),
+]
+    for red in [:+, :min]
+        @eval wd!(f::AbstractArray{<:Any,$N}, w, k::AbstractArray{<:Any,$(N+1)}, ::typeof($red)) = @tullio ($red) $kernel
+    end
+
+    @eval function wd_adjoint!(_f::AbstractArray{T,$N}, w, k::AbstractArray{<:Any,$(N+1)}, ::typeof(min)) where {T}
+        f = ArgMinArray(similar(_f, Int), _f)
+        @tullio (argmin_inner) $kernel_argmin init=(0, typemax(T))
+        j_min = f.idx
+        function wd_pullback_w!(dw, Δ)
+            @tullio $kernel_dw
+        end
+        function wd_pullback_k!(dk, Δ)
+            @tullio (+) $kernel_dk
+        end
+        return f.min, wd_pullback_w!, wd_pullback_k!
+    end
 end
