52cd417f3a564e5ae956cbbb33f15c1f0a148466
Simeon Schaub <simeondavidschaub99@gmail.com>
2021-01-14 10:00:10 +0100

    add roc curves

--- script/plots.jl
+++ script/plots.jl
@@ -1,6 +1,7 @@
-using Arrow, DataFrames
+using Arrow, DataFrames, Flux
 using PyPlot
 using Printf
+using Trapz
 
 _reshape(a, dims...) = invoke(Base._reshape, Tuple{AbstractArray, Base.Dims}, a, Base._reshape_uncolon(a, dims))
 
@@ -10,7 +11,7 @@ all_features = DataFrame(Arrow.Table(joinpath(output_dir, "all_features.arrow"))
 fig1, axs1 = subplots(ncols=2, nrows=2, figsize=(12, 9))
 linestyles = (train = "-", test = "--", validation="-.")
 for ((kind,), df) in pairs(groupby(all_features, :kind))
-    foreach(
+    kind !== :test && foreach(
         pairs(groupby(df, :output_expected, sort=true)),
         [:C0, :C1, :C2, :C3],
     ) do ((class,), df), color
@@ -42,9 +43,11 @@ for ((kind,), df) in pairs(groupby(all_features, :kind))
         ax.text(j-1, i-1, @sprintf("%.3g", confusion_mat[i, j]), ha=:center, va=:center)
     end
     ax.set_xlabel("predicted output")
-    xticks(eachindex(classes).-1, classes)
+    ax.set_xticks(eachindex(classes).-1)
+    ax.set_xticklabels(classes)
     ax.set_ylabel("true output")
-    yticks(eachindex(classes).-1, classes)
+    ax.set_yticks(eachindex(classes).-1)
+    ax.set_yticklabels(classes)
     fig.colorbar(img)
     fig.suptitle(kind)
     fig.savefig(joinpath(output_dir, "confusion_matrix_$kind.pdf"))
@@ -64,10 +67,44 @@ for ((kind,), df) in pairs(groupby(all_features, :kind))
     #fig.tight_layout()
     #fig.savefig(joinpath(output_dir, "input_features_hist_$kind.pdf"))
 
-    #predictions_positives = df[df.output_expected .=== :Hbb, :output_predicted_Hbb]
-    #fig, ax = subplots()
-    #ax.hist(predictions_positives, cumulative=true, bins=30, density=true, histtype=:step, orientation=:horizontal)
-    #fig.savefig(joinpath(output_dir, "roc_curve_$kind.pdf"))
+    fig_eff, axs_eff = subplots(ncols=2, nrows=2, figsize=(12, 9))
+    fig_roc, axs_roc = subplots(ncols=2, nrows=2, figsize=(12, 9))
+    for (ax_roc, ax_eff, class) in zip(axs_roc, axs_eff, classes)
+        predictions_positives = df[df.output_expected .=== class, Symbol(:output_predicted_, class)]
+        predictions_background = df[df.output_expected .!== class, Symbol(:output_predicted_, class)]
+        eff_s, = ax_eff.hist(
+            predictions_positives;
+            cumulative=true, bins=range(0, 1; length=30), density=true, histtype=:step,
+            label="signal efficiency"
+        )
+        eff_bg, = ax_eff.hist(
+            predictions_background;
+            cumulative=true, bins=range(0, 1; length=30), density=true, histtype=:step,
+            label="background efficiency",
+        )
+        ax_eff.legend()
+        ax_eff.set_ylabel("efficiency")
+        ax_eff.set_xlabel("1 - specificity")
+        ax_eff.set_title("$class")
+
+        x, y = 1 .- eff_s, eff_bg
+        ax_roc.plot(x, y)
+        idx = sortperm(x)
+        auc = trapz(x[idx], y[idx])
+        ax_roc.text(.2, .2, @sprintf("AUC = %.3f", auc); fontsize=16)
+        ax_roc.set_xlim(0, 1)
+        ax_roc.set_ylim(0, 1)
+        ax_roc.set_ylabel("1 - background efficiency")
+        ax_roc.set_xlabel("signal efficiency")
+        ax_roc.set_title("$class")
+    end
+    fig_eff.suptitle("Efficiencies - $kind")
+    fig_eff.tight_layout()
+    fig_eff.savefig(joinpath(output_dir, "efficiencies_$kind.pdf"))
+
+    fig_roc.suptitle("ROC curves - $kind")
+    fig_roc.tight_layout()
+    fig_roc.savefig(joinpath(output_dir, "roc_curves_$kind.pdf"))
 end
 
 foreach(axs1, classes) do ax1, class_predicted
@@ -78,4 +115,3 @@ end
 #fig1.suptitle("Predicted Hbb")
 fig1.tight_layout()
 fig1.savefig(joinpath(output_dir, "output_features_hist.pdf"))
-
