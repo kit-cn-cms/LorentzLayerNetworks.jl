78d4cccca4e183d18589ad701302d61d17c01375
Simeon Schaub <simeondavidschaub99@gmail.com>
2021-01-25 12:17:12 +0100

    compare scalar features

--- script/run.jl
+++ script/run.jl
@@ -19,84 +19,88 @@ include("save_model.jl")
 basedir = "/local/scratch/ssd/sschaub/h5files/ttH_sebastian-processed/"
 h5files = joinpath.(basedir, ["ttH", "ttbb", "ttcc", "ttlf"] .* "_lola.h5")
 
-feature_names = Vars.all_features
-inputs = extract_cols(h5files, feature_names) .|> Float32
+for scalar_features in [[String[]]; Base.vect.(Vars.scalar_features)]
+    @show scalar_features
+    feature_names = [Vars.jets; Vars.tight_lepton; Vars.jet_csv; scalar_features]
+    inputs = extract_cols(h5files, feature_names) .|> Float32
 
-using .Vars: classes
-outputs = Symbol.(extract_cols(h5files, ["class_label"]))
-outputs_onehot = Flux.onehotbatch(vec(outputs), classes)
+    classes = Vars.classes
+    outputs = Symbol.(extract_cols(h5files, ["class_label"]))
+    outputs_onehot = Flux.onehotbatch(vec(outputs), classes)
 
-weights = prod(Float32, extract_cols(h5files, Vars.weights); dims=1)
-total_weights = scale_weights(weights, outputs)
+    weights = prod(Float32, extract_cols(h5files, Vars.weights); dims=1)
+    total_weights = scale_weights(weights, outputs)
 
-ds_train, ds_validation, ds_test = split_datasets(
-    gpu,
-    (inputs, outputs_onehot, total_weights, axes(inputs, 2)),
-    [.75 * .8, .75 * .2, .25],
-)
-idx_train, idx_validation, idx_test = last.((ds_train, ds_validation, ds_test))
-ds_train = DataLoader(ds_train; batchsize=1024, shuffle=true)
-ds_validation = DataLoader(ds_validation; batchsize=1024, shuffle=false)
-ds_test = DataLoader(ds_test; batchsize=1024, shuffle=false)
+    ds_train, ds_validation, ds_test = split_datasets(
+        gpu,
+        (inputs, outputs_onehot, total_weights, axes(inputs, 2)),
+        [.75 * .8, .75 * .2, .25],
+    )
+    idx_train, idx_validation, idx_test = last.((ds_train, ds_validation, ds_test))
+    ds_train = DataLoader(ds_train; batchsize=1024, shuffle=true)
+    ds_validation = DataLoader(ds_validation; batchsize=1024, shuffle=false)
+    ds_test = DataLoader(ds_test; batchsize=1024, shuffle=false)
 
-m = 15
-n_jets = length([Vars.jets; Vars.tight_lepton]) ÷ 4
-n_inputs = size(inputs, 1)
-n_outputs = length(classes)
-model = build_model(;
-    m, n_jets, n_inputs, n_outputs,
-    w_d_reducers = ((+) => 2, min => 2),
-    #w_d_reducers = (min => 4,),
-    neurons = [1024, 2048, 512, 512],
-    activation = _leakyrelu,
-    dropout = Dropout(0.5),
-) |> gpu
-
-optimizer = ADAM(5e-5)
-
-penalty = l2_penalty(model)
-loss(ŷ, y; weights) = Flux.logitcrossentropy(ŷ, y; agg = x -> weighted_mean(x, weights)) + 1f-5 * penalty()
-
-measures = (; loss=st->st.loss, accuracy=st->accuracy(softmax(st.ŷ), st.y))
-
-recorded_measures = DataFrame()
-
-# pretrain Lo+CoLa
-n_outputs_lola = (n_jets + m) * (3 + 4) + length(Vars.jet_csv)
-_model = Chain(
-    model[1:end-1]...,
-    #x -> view(x, 1:n_outputs_lola, :),
-    x -> x[1:n_outputs_lola, :],
-    build_dnn(;
-        n_inputs = n_outputs_lola,
+    m = 15
+    n_jets = length([Vars.jets; Vars.tight_lepton]) ÷ 4
+    n_inputs = size(inputs, 1)
+    n_outputs = length(classes)
+    model = build_model(;
+        m, n_jets, n_inputs, n_outputs,
+        w_d_reducers = ((+) => 2, min => 2),
         neurons = [1024, 2048, 512, 512],
-        n_outputs,
         activation = _leakyrelu,
         dropout = Dropout(0.5),
-    ) |> gpu,
-)
-train!(;
-    model=_model, ds_train, ds_test, ds_validation,
-    loss, optimizer, measures, recorded_measures,
-    max_epochs=20, min_epochs=50, early_stopping_n=10, early_stopping_percentage=2,
-)
+    ) |> gpu
 
-# main training
-train!(;
-    model, ds_train, ds_test, ds_validation,
-    loss, optimizer, measures, recorded_measures,
-    max_epochs=120, min_epochs=50, early_stopping_n=7, early_stopping_percentage=2,
-)
+    optimizer = ADAM(5e-5)
 
-save_model(;
-    output_dir = "/work/sschaub/JuliaForHEP/lola+scalars12/",
-    fig,
-    recorded_measures,
-    model,
-    inputs,
-    feature_names,
-    outputs,
-    weights,
-    total_weights,
-    idx_train, idx_test, idx_validation,
-)
+    penalty = l2_penalty(model)
+    loss(ŷ, y; weights) = Flux.logitcrossentropy(ŷ, y; agg = x -> weighted_mean(x, weights)) + 1f-5 * penalty()
+
+    measures = (; loss=st->st.loss, accuracy=st->accuracy(softmax(st.ŷ), st.y))
+
+    recorded_measures = DataFrame()
+
+    # pretrain Lo+CoLa
+    n_outputs_lola = (n_jets + m) * (3 + 4) + length(Vars.jet_csv)
+    _model = Chain(
+        model[1:end-1]...,
+        #x -> view(x, 1:n_outputs_lola, :),
+        x -> x[1:n_outputs_lola, :],
+        build_dnn(;
+            n_inputs = n_outputs_lola,
+            neurons = [1024, 2048, 512, 512],
+            n_outputs,
+            activation = _leakyrelu,
+            dropout = Dropout(0.5),
+        ) |> gpu,
+    )
+    train!(;
+        model=_model, ds_train, ds_test, ds_validation,
+        loss, optimizer, measures, recorded_measures,
+        max_epochs=20, min_epochs=50, early_stopping_n=10, early_stopping_percentage=2,
+    )
+
+    # main training
+    train!(;
+        model, ds_train, ds_test, ds_validation,
+        loss, optimizer, measures, recorded_measures,
+        max_epochs=120, min_epochs=50, early_stopping_n=7, early_stopping_percentage=2,
+    )
+
+    name = isempty(scalar_features) ? "none" : scalar_features[]
+
+    save_model(;
+        output_dir = "/work/sschaub/JuliaForHEP/lola+$name/",
+        fig,
+        recorded_measures,
+        model,
+        inputs,
+        feature_names,
+        outputs,
+        weights,
+        total_weights,
+        idx_train, idx_test, idx_validation,
+    )
+end
