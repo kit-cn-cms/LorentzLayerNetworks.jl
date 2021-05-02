ca510ae4e8218cd1e89dc6a71d17cf0b65d68407
Simeon Schaub <simeondavidschaub99@gmail.com>
2021-05-02 13:51:13 +0200

    add plot script for losses

---
+++ script/plot_recorded_measures.jl
@@ -0,0 +1,19 @@
+using Arrow, DataFrames
+using PyPlot
+
+include("plot_styles.jl")
+
+basedir = "/work/sschaub/JuliaForHEP/final_plotting/"
+tex = true
+
+output_dir = joinpath(basedir, "lola+all_1")
+recorded_measures = DataFrame(Arrow.Table(joinpath(output_dir, "recorded_measures.arrow")))
+
+fig, ax = subplots(figsize=(12, 9))
+ax.plot([recorded_measures.train_loss recorded_measures.test_loss recorded_measures.validation_loss])
+ax.set_title("Loss Values")
+ax.set_xlabel("Epoch")
+ax.set_ylabel("Loss")
+ax.legend(["Training Loss", "Testing Loss", "Validation Loss"])
+
+fig.savefig(joinpath(output_dir, "losses.pdf"))
